ec2ce60b020494344fca3dc662855dd9db774f02
Anthony Garland <garland3@gmail.com>
2015-10-07 13:15:51 -0400

    solved GPU problem, I think

--- SIMP_elastic_heat_gradient/FE_elasticV2.m
+++ SIMP_elastic_heat_gradient/FE_elasticV2.m
@@ -154,7 +154,7 @@ if(settings.useGPU ==1)
     K_ff_gpu = gpuArray(K_ff);
     F_f_gpu = gpuArray(F_f);
     T_gpu = K_ff_gpu\F_f_gpu;
-    T = gather(T_gpu);
+    T(Free) = gather(T_gpu);
 else
     % normal matrix solve
      T(Free) = K_ff \ F_f;
